cea767a04c496358e0da1bdb0254416fb05f5413
Kenslee Moy <KensleeMoy@u.boisestate.edu>
2020-09-10 17:39:12 -0600

    finished fibonacci sequence

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 
+Notes: 
+Fibonacci starts at fib(0) = 0, so to calculate the 3rd term, it will display terms 0, 1, 2, 3 to give you the fib(3) = 2.
 
 ** Resources:
 (Leibniz formula) https://stackoverflow.com/questions/18036367/leibniz-formula-for-%CF%80-is-this-any-good-python
 (Checking in Leibniz for valid input) https://stackoverflow.com/questions/17292545/how-to-check-if-the-input-is-a-number-or-not-in-c
 (Monte Carlo formula) https://www.geeksforgeeks.org/estimating-value-pi-using-monte-carlo/
+(Fibonacci formula) https://www.programiz.com/c-programming/examples/fibonacci-series
+(Catch2 Test setup) https://github.com/catchorg/Catch2
--- src/fibonacci.c
+++ src/fibonacci.c
@@ -5,19 +5,34 @@ int main(int argc, char *argv[]) {
     int input;
     char* next;
     int term;
+    int firstT = 0;
+    int secondT = 1;
+    int nextTerm = 0;
+    int result = 0;
 
     if (argc == 2) {
         input = strtol(argv[1], &next, 10);
 
-        if ((next != argv[1]) && (*next == '\0')) {
+        if ((next != argv[1]) && (*next == '\0') && (input >= 0)) {
             term = input;
+            
+            for(int i = 0; i <= term; i++) {
+                printf("%d, ", firstT);
+                if (i == term) {
+                    result = firstT;
+                }
+                nextTerm = firstT + secondT;
+                firstT = secondT;
+                secondT = nextTerm;
+            }
+            printf("\nThe [%d] number in the Fibonacci sequence: %d\n", term, result);
             return 0;
         } else {
-            printf("USAGE: ./piLeib <integer>\n");
+            printf("USAGE: ./fibonacci <natural integer number>\n");
             return -1;
         }
     } else {
-        printf("USAGE: ./piLeib <integer>\n");
+        printf("USAGE: ./fibonacci <natural integer number>= 0>\n");
         return -1;
     }
 }
