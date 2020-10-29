4d790d70f8a33f5cf13008a19ad812271a5a1824
Kenslee Moy <Kensleemoy@u.boisestate.edu>
2020-10-29 11:17:19 -0600

    ah

--- src/fib-openmp.c
+++ src/fib-openmp.c
@@ -45,13 +45,13 @@ long fib(long n) {
         return n;
 
     //Term 1
-    #pragma omp parallel task shared(i) if(n > 33)
     // #pragma omp task shared(i)
+    #pragma omp parallel task shared(i) if(n > 33)
     i = fib(n-1);
 
     //Term 2
-    #pragma omp parallel task shared(j) if(n > 33)
     // #pragma omp task shared(j)
+    #pragma omp parallel task shared(j) if(n > 33)
     j = fib(n-2);
 
     //Waiting for tasks to finish before using the returned values
