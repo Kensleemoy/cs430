13634088a89588425578cd24d29dd5325faaf4db
Kenslee Moy <Kensleemoy@u.boisestate.edu>
2020-11-02 18:57:40 -0700

    testing

--- src/fib-openmp.c
+++ src/fib-openmp.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+#import <stdint.h>
 #include <omp.h>
 #include <time.h>
 
@@ -8,53 +8,38 @@
 long fib(long n);
 
 int main(int argc, char *argv[]) {
-    int input, threads, time;
+    int input;
     char* next;
-    int term, nextTerm, firstT, secondT;
+    long term;
     long result = 0;
     clock_t start, end;
-    firstT = 0;
-    secondT = 1;
+    int time;
 
     //Ensuring the correct number of arguments are there
-    if (argc == 3) {
+    if (argc == 2) {
 
-        //Grabbing input as decimal number (base 10)
+        //Grabbing input
         input = strtol(argv[1], &next, 10);
 
         //Error handling to ensure valid input
         if ((next != argv[1]) && (*next == '\0') && (input >= 0)) {
-            term = input;
-            threads = strtol(argv[2], NULL, 10);
+            term = (long)input;
+
+            omp_set_num_threads(omp_get_num_procs());
 
             //Starts Recursive call
-            // #pragma omp parallel shared(term, result) 
-            // {
-            //     #pragma omp single 
-            //     {
-            //         start = clock();
-            //         result = fib(term);
-            //         end = clock();
-            //         time = (end - start) * 1000 / CLOCKS_PER_SEC;
-            //     }
-            // }
-
-            start = clock();
-            #pragma omp parallel shared(result, firstT, nextTerm, secondT, term) for num_threads(threads)
+            #pragma omp parallel shared(term, result) 
             {
-                for(int i = 0; i <= term; i++) {
-                    if (i == term) {
-                        result = firstT;
-                    }
-                    nextTerm = firstT + secondT;
-                    firstT = secondT;
-                    secondT = nextTerm;
+                #pragma omp single 
+                {
+                    start = clock();
+                    result = fib(term);
+                    end = clock();
+                    time = (end - start) * 1000 / CLOCKS_PER_SEC;
                 }
             }
-            end = clock();
-            time = (end - start) * 1000 / CLOCKS_PER_SEC;
             printf("Time: %d\n", time);
-            printf("The [%d] number in the Fibonacci sequence: %d\n", term, result);
+            printf("The [%d] number in the Fibonacci sequence: %ld\n", (int)term, result);
             return 0;
 
         } else {
@@ -68,27 +53,27 @@ int main(int argc, char *argv[]) {
 }
 
 //Recursive function
-// long fib(long n) {
-//     long i, j;
+long fib(long n) {
+    long i, j;
 
     //Stop requirement --> stop creating new threads once n is a low enough value
-    // if(n <= 1) {
-    //     return n;
-    // } else if(n <= 33) {
-    //     return(fib(n-1)+fib(n-2));
-    // } else {
-    //     #pragma omp task shared(i) if(n > 50)
-    //     {
-    //         i = fib(n-1);
-    //     }
+    if(n <= 1) {
+        return n;
+    } else if(n <= 33) {
+        return(fib(n-1)+fib(n-2));
+    } else {
+        #pragma omp task shared(i) if(n > 33)
+        {
+            i = fib(n-1);
+        }
 
-    //     j = fib(n-2);
+        j = fib(n-2);
         
-    //     #pragma omp taskwait
-    //     {
-    //         return i+j;
-    //     }
-    // }
+        #pragma omp taskwait
+        {
+            return i+j;
+        }
+    }
     //Term 1
     // #pragma omp task shared(i)
     // #pragma omp parallel task shared(i) if(n > 33)
@@ -102,4 +87,5 @@ int main(int argc, char *argv[]) {
 
     //Waiting for tasks to finish before using the returned values
     //Adds term1 + term2
-// }
+    
+}
