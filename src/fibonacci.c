aee288943b49968420d0f0bd5fc71a72d6786dbb
Kenslee Moy <Kensleemoy@u.boisestate.edu>
2020-11-20 17:06:11 -0700

    ints

--- src/fibonacci.c
+++ src/fibonacci.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
     long nextTerm = 0;
     long result = 0;
     clock_t start, end;
-    double time;         //this will be used for timing for the report
+    int time;         //this will be used for timing for the report
 
     if (argc == 2) {
         input = strtol(argv[1], &next, 10);
@@ -30,9 +30,9 @@ int main(int argc, char *argv[]) {
                 secondT = nextTerm;
             }
             end = clock();
-            printf("%d", (double)end);
-            time = ((double)(end - start)) / (double)CLOCKS_PER_SEC;
-            printf("Time: %f\n", time);
+            printf("%d", end);
+            time = (end - start)*1000 / CLOCKS_PER_SEC;
+            printf("Time: %d\n", time);
             printf("The [%ld] number in the Fibonacci sequence: %ld\n", term, result);
             return 0;
         } else {
