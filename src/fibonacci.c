d20c574e59d5f0e89d855e485cbc676988e32906
Kenslee Moy <Kensleemoy@u.boisestate.edu>
2020-11-20 17:50:07 -0700

    trying to tme serial again

--- src/fibonacci.c
+++ src/fibonacci.c
@@ -30,9 +30,9 @@ int main(int argc, char *argv[]) {
                 secondT = nextTerm;
             }
             end = clock();
-            time = (double)(end - start)/CLOCKS_PER_SEC;
+            time = ((double)(end - start)) / ((double)CLOCKS_PER_SEC);
 
-            printf("Time: %LF\n", time*10000);
+            printf("Time: %f\n", (double)time);
             printf("The [%ld] number in the Fibonacci sequence: %ld\n", term, result);
             return 0;
         } else {
